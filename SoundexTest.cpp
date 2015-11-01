6e7fe778e830d9bb9ec2a06aa33a00d51944b2b8
ordinary-developer <merely.ordinary.developer@gmail.com>
2015-11-01 10:42:35 +0300

    add the "PadsWithZerosToEnsureThreeDigits" test
    add the next test
    
    after the commit
    - project has 2 tests
      The last is "PadsWithZerosToEnsureThreeDigits"
    - all tests pass

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -2,9 +2,18 @@
 
 #include "gmock/gmock.h"
 
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
-    Soundex soundex;
+class SoundexEncoding: public testing::Test {
+    public:
+        Soundex soundex;
+};
+
+TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
     auto encoded = soundex.encode("A");
-    ASSERT_THAT(encoded, testing::Eq("A"));
+    ASSERT_THAT(encoded, testing::Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, PadsWithZerosToEnsureTreeDigits) {
+    auto encoded = soundex.encode("I");
+    ASSERT_THAT(encoded, testing::Eq("I000"));
 }
 
