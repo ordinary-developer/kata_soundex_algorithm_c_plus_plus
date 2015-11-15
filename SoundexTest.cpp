a7968fa8fb9d710153c949e4bc1ab411f0b83f6d
ordinary-developer <merely.ordinary.developer@gmail.com>
2015-11-15 09:37:44 +0300

    add the "PadsWithZerosToEnsureThreeDigits" test
    add the test that verifies the padding with zeros
    for ensuring three digits
    
    after this commit:
    - the project contains 2 tests that pass

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -3,9 +3,21 @@
 #include "gmock/gmock.h"
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
+    // arrange 
     Soundex soundex;
-
+    // act
     auto encoded = soundex.encode("A");
-
-    ASSERT_THAT(encoded, testing::Eq("A"));
+    // assert
+    ASSERT_THAT(encoded, testing::Eq("A000"));
 }
+
+TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
+    // [arrange]
+    Soundex soundex;
+    // [act]
+    auto encoded = soundex.encode("I");
+    // [assert]
+    ASSERT_THAT(encoded, testing::Eq("I000"));
+}
+
+
