7846cf18746bf217a9b019ba0732ab5ed31a5622
ordinary-developer <merely.ordinary.developer@gmail.com>
2015-11-15 09:52:22 +0300

    add the "ReplacesCons-sWithApproprDigits" test
    add the test that verifies replacing consonants with
    the appropriate digits (according to specifications)
    
    after this commit:
    - the project contains 3 tests that pass

--- Soundex.cpp
+++ Soundex.cpp
@@ -2,12 +2,32 @@
 
 class Soundex { 
     public:
+        static const size_t MaxCodeLength { 4 };
+
         std::string encode(const std::string& word) const {
-            return zeroPad(word);
+            return zeroPad(head(word) + encodedDigits(word));
         }
 
     private:
         std::string zeroPad(const std::string& word) const {
-            return word + "000";
+            auto zerosNeeded = MaxCodeLength - word.length();
+            return word + std::string(zerosNeeded, '0');
+        }
+
+        std::string head(const std::string& word) const {
+            return word.substr(0, 1);
+        }
+
+        std::string encodedDigits(const std::string& word) const {
+            if (word.length() > 1) {
+                return encodedDigit();
+            }
+            else {
+                return "";
+            }
+        }
+
+        std::string encodedDigit() const {
+            return "1";
         }
 };
--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -8,17 +8,18 @@ class SoundexEncoding: public testing::Test {
 };
 
 TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
-    // arrange - act
-    auto encoded = soundex.encode("A");
-    // assert
-    ASSERT_THAT(encoded, testing::Eq("A000"));
+    // arrange - act - assert
+    ASSERT_THAT(soundex.encode("A"), testing::Eq("A000"));
 }
 
 TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
-    // [arrange - act]
-    auto encoded = soundex.encode("I");
-    // [assert]
-    ASSERT_THAT(encoded, testing::Eq("I000"));
+    // [arrange] - [act] - [assert]
+    ASSERT_THAT(soundex.encode("I"), testing::Eq("I000"));
+}
+
+TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
+    // [ARRANGE] - [ACT] - [ASSERT]
+    ASSERT_THAT(soundex.encode("Ab"), testing::Eq("A100"));
 }
 
 
