170bc1a67bc702bea0333192eec720a5a8dedaab
ordinary-developer <merely.ordinary.developer@gmail.com>
2015-11-15 09:41:07 +0300

    refactor the previous tests
    make a test refactoring to use the TEST_F macro
    
    after this commit:
    - the project contains 2 tests that pass

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -2,19 +2,20 @@
 
 #include "gmock/gmock.h"
 
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
-    // arrange 
-    Soundex soundex;
-    // act
+class SoundexEncoding: public testing::Test {
+    public:
+        Soundex soundex;
+};
+
+TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
+    // arrange - act
     auto encoded = soundex.encode("A");
     // assert
     ASSERT_THAT(encoded, testing::Eq("A000"));
 }
 
-TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
-    // [arrange]
-    Soundex soundex;
-    // [act]
+TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
+    // [arrange - act]
     auto encoded = soundex.encode("I");
     // [assert]
     ASSERT_THAT(encoded, testing::Eq("I000"));
