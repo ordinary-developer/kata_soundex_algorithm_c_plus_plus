c46da91397b938f0288c98c83808edaee844eed8
ordinary-developer <merely.ordinary.developer@gmail.com>
2015-11-15 09:26:37 +0300

    BEGIN THE NEXT KATA
    begin the next kata, create all necessary files
    
    after this commit:
    - the project has one silly test that passes

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -2,65 +2,6 @@
 
 #include "gmock/gmock.h"
 
-class SoundexEncoding: public testing::Test {
-    public:
-        Soundex soundex;
-};
-
-TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
-    auto encoded = soundex.encode("A");
-    ASSERT_THAT(encoded, testing::Eq("A000"));
-}
-
-TEST_F(SoundexEncoding, PadsWithZerosToEnsureTreeDigits) {
-    auto encoded = soundex.encode("I");
-    ASSERT_THAT(encoded, testing::Eq("I000"));
-}
-
-TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
-    ASSERT_THAT(soundex.encode("Ax"), testing::Eq("A200"));
-}
-
-TEST_F(SoundexEncoding, IgnoreNonAlphabetics) {
-    ASSERT_THAT(soundex.encode("A#"), testing::Eq("A000"));
-}
-
-TEST_F(SoundexEncoding, ReplacesMultipleConsonantsWithDigits) {
-    ASSERT_THAT(soundex.encode("Acdl"), testing::Eq("A234"));
-}
-
-TEST_F(SoundexEncoding, LimitsLengthToFourCharacters) {
-    ASSERT_THAT(soundex.encode("Dcdlb").length(), testing::Eq(4u));
-}
-
-TEST_F(SoundexEncoding, IgnoresVowelLikeLetters) {
-    ASSERT_THAT(soundex.encode("Baeiouhycdl"), testing::Eq("B234"));
-    ASSERT_THAT(soundex.encode("BaAeEiIoOuUhHyYcdl"), testing::Eq("B234"));
-}
-
-TEST_F(SoundexEncoding, CombinesDuplicateEncodings) {
-    ASSERT_THAT(soundex.encodedDigit('b'), 
-                testing::Eq(soundex.encodedDigit('f')));
-    ASSERT_THAT(soundex.encodedDigit('c'), 
-                testing::Eq(soundex.encodedDigit('g')));
-    ASSERT_THAT(soundex.encodedDigit('d'), 
-                testing::Eq(soundex.encodedDigit('t')));
-
-    ASSERT_THAT(soundex.encode("Abfcgdt"), testing::Eq("A123"));
-}
-
-TEST_F(SoundexEncoding, UppercasesFirstLetter) {
-    ASSERT_THAT(soundex.encode("abcd"), testing::StartsWith("A"));
-}
-
-TEST_F(SoundexEncoding, IgnoresCaseWhenEncodingConsonants) {
-    ASSERT_THAT(soundex.encode("BCDL"), testing::Eq(soundex.encode("Bcdl")));
-}
-
-TEST_F(SoundexEncoding, CombinesDuplicateCodesWhen2ndLetterDuplicates1st) {
-    ASSERT_THAT(soundex.encode("Bbcd"), testing::Eq("B230"));
-}
-
-TEST_F(SoundexEncoding, DoesNotCombineDuplicateEncodingsSeparatedByVowels) {
-    ASSERT_THAT(soundex.encode("Jbob"), testing::Eq("J110"));
+TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
+    Soundex soundex;
 }
